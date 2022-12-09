ebda655acc04b55f7c9085033c714b06fee27aa1
sigkill <blakat360@gmail.com>
2022-12-09 02:44:24 +0000

    made rzf a proper package

--- pkgs/rzf/default.nix
+++ pkgs/rzf/default.nix
@@ -1,14 +1,28 @@
 { pkgs ? import <nixpkgs> }:
 
-pkgs.writeShellApplication {
+pkgs.stdenv.mkDerivation rec {
   name = "rzf";
-  runtimeInputs = with pkgs; [ fzf rofi ];
-  text = ''
-    if [ "$TERM" == 'screen' ]; then
-      fzf-tmux
-    else
-    	rofi -dmenu
-    fi
-  '';
+
+  src = pkgs.writeShellApplication {
+    name = "rzf";
+    runtimeInputs = with pkgs; [ fzf rofi ];
+    text = ''
+      if [ "$TERM" == 'screen' ]; then
+        fzf-tmux
+      else
+      	rofi -dmenu
+      fi
+    '';
+  };
+
+  meta = with pkgs.lib; {
+    description = ''
+      Fuzzy finder you can use locally, and in remote sessions.
+        e.g. `printf "hello\nworld" | rzfi`
+      	fzf-tmux is used when in tmux, rofi -dmenu is used outside of tmux
+    '';
+		platforms = platforms.all;
+  };
+
 }
 
